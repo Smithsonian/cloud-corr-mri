860091a6dfc69ca0be54567f3dc76e8b82ebb6ee
Greg Lindahl <glindahl@cfa.harvard.edu>
2021-01-20 05:39:18 +0000

    prettier

--- scripts/genmachines-onenode.py
+++ scripts/genmachines-onenode.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+
 '''
 Needs to run on python 2.7 because that's what's currently inside the
 Singularity image
@@ -12,7 +13,10 @@ oversubscription and high loads.
 This genmachines works only on a single machine, and seems to mostly keep
 the load down to something reasonable. You still may need to oversubscribe,
 if there are too many baselines.
+
+export DIFX_MPIRUNOPTIONS="--oversubscribe"  # OpenMPI
 '''
+
 
 from __future__ import print_function
 
@@ -21,29 +25,6 @@ import os
 import os.path
 import math
 import socket
-import multiprocessing
-
-base = sys.argv[1]
-
-if not os.path.exists(base + '.input'):
-    raise ValueError(base + '.input does not exist')
-
-hostname = socket.gethostname()  # can't use localhost, mpifxcorr will crash
-
-# what's the right computation?
-# 1 fxmanager
-# S datastreams -- ACTIVE DATASTREAMS from .input
-# C Cores -- ACTIVE BASELINES from .input
-
-with open(base + '.input', 'r') as fd:
-    for line in fd:
-        if 'ACTIVE DATASTREAMS:' in line:
-            datastreams = line.rsplit(None, 1)[1]  # py2.7
-        if 'ACTIVE BASELINES:' in line:
-            baselines = line.rsplit(None, 1)[1]  # py2.7
-
-nodes = 1 + int(datastreams) + int(baselines)
-
 
 def _core_count():
     try:
@@ -52,21 +33,40 @@ def _core_count():
         try:
             return os.cpu_count()
         except AttributeError:
-            try:
-                return multiprocessing.cpu_count()  # py2.7
-            except NotImplementedError:
-                return 48
-                
+            # py2.7
+            import multiprocessing
+            return multiprocessing.cpu_count()
 
-threads = int(math.ceil(_core_count() / int(baselines)))
 
-if threads < 1:
-    print('genmachines: WARNING: node will be oversubscribed', file=sys.stderr)
-    threads = 1
+for base in sys.argv[1:]:
+    if base.endswith('.input'):
+        base = base[:-len('.input')]
 
-with open(base + '.machines', 'w') as fd:
-    for _ in range(nodes):
-        print(hostname, file=fd)
+    if not os.path.exists(base + '.input'):
+        raise ValueError(base + '.input does not exist')
 
-with open(base + '.threads', 'w') as fd:
-    print('NUMBER OF CORES:    {}\n{}'.format(1, threads), file=fd)
+    hostname = socket.gethostname()
+
+    with open(base + '.input', 'r') as fd:
+        for line in fd:
+            if 'ACTIVE DATASTREAMS:' in line:
+                datastreams = line.rsplit(None, 1)[1]  # py2.7
+            if 'ACTIVE BASELINES:' in line:
+                baselines = line.rsplit(None, 1)[1]  # py2.7
+
+    nodes = 1 + int(datastreams) + int(baselines)
+    threads = int(math.ceil(_core_count() / int(baselines)))
+    threads = max(threads, 1)
+    cores = 1 + int(datastreams) + threads * int(baselines)  # 1 for fxmanager
+
+    print(base,
+          'datastreams='+datastreams,
+          'baselines='+baselines, 'threads='+str(threads),
+          'cores='+str(cores))
+
+    with open(base + '.machines', 'w') as fd:
+        for _ in range(nodes):
+            print(hostname, file=fd)
+
+    with open(base + '.threads', 'w') as fd:
+        print('NUMBER OF CORES:    {}\n{}'.format(1, threads), file=fd)
