12131811aed390edfa4e3d02f09cabafc1cb4a92
Greg Lindahl <glindahl@cfa.harvard.edu>
2021-09-07 02:42:09 +0000

    new utility

---
+++ transport/watch-network.py
@@ -0,0 +1,69 @@
+import time
+import subprocess
+from collections import defaultdict
+
+def parse_netstat_s(s):
+    ret = defaultdict(dict)
+
+    for line in s.splitlines():
+        if not line.startswith(' ') and ':' in line:
+            section = line.split(':', 1)[0]
+            continue
+        if line.startswith('    '):
+            #print(line)
+            #print(line[4:])
+            count, message = line[4:].split(' ', 1)
+            if not count.isdigit():
+                # example: ICMP output histogram
+                continue
+            count = int(count)
+            ret[section][message] = count
+
+    ret['time'] = time.time()
+    return ret
+
+
+def measure_netstat_s():
+    completed = subprocess.run(('netstat', '-s'), stdout=subprocess.PIPE)
+    return parse_netstat_s(completed.stdout.decode())
+
+
+def print_one(stat, data, previous_data, delta_t):
+    section, thing = stat
+    if thing not in data[section]:
+        print('{} {} appears to not be a valid thing'.format(section, thing))
+        print(data[section])
+        return
+    delta = data[section][thing] - previous_data[section][thing]
+    if delta > 0:
+        print(delta, section, thing)
+
+
+def loop():
+    previous_data = None
+    while True:
+        data = measure_netstat_s()
+        if previous_data:
+            delta_t = data['time'] - previous_data['time']
+            for stat in (
+                    # not errors
+                    ('Udp', 'packets received'),
+                    ('Udp', 'packets sent'),
+                    # errors
+                    ('Udp', 'packet receive errors'),
+                    ('Udp', 'receive buffer errors'),
+                    ('Udp', 'send buffer errors'),
+
+                    ('Ip', 'dropped because of missing route'),
+                    ('Ip', 'packets reassembled ok'),
+                    ('Ip', 'incoming packets discarded'),
+            ):
+                print_one(stat, data, previous_data, delta_t)
+        previous_data = data
+        time.sleep(1.0)
+        print('')
+        continue
+
+
+if __name__ == '__main__':
+    loop()
