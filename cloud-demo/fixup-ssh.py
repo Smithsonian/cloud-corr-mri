4da66ac9165348a571064e7226c68e37f1134a33
Greg Lindahl <lindahl@pbm.com>
2022-08-21 02:55:07 +0000

    debugging

--- cloud-demo/fixup-ssh.py
+++ cloud-demo/fixup-ssh.py
@@ -1,23 +1,30 @@
 import subprocess
 import shlex
+import sys
 
 import ray
 
 ray.init(address='auto')
 
-#ips = [x['NodeName'] for x in ray.nodes()]
-ips = [x['NodeManagerHostname'] for x in ray.nodes()]
-
-# copy my public key to my authorized keys                                                                                                                                   
+# copy my public key to my authorized keys, so I can ssh to myself
 subprocess.run(shlex.split('cp .ssh/id_rsa.pub .ssh/authorized_keys'))
 
-# gather host keys                                                                                                                                                           
+print('Getting node ips')
+sys.stdio.flush()
+ips = [x['NodeManagerHostname'] for x in ray.nodes()]
+
+print('Gathering host keys')
 for ip in ips:
+    print(' ', ip)
+    sys.stdio.flush()
     cmd = 'ssh -o StrictHostKeyChecking=no '+ip+' echo foo'
     subprocess.run(shlex.split(cmd))
 
-# copy over my keys, whicih are already in everyone else's authorized keys                                                                                                   
+# copy over my keys, whicih are already in everyone else's authorized keys (?)
+print('Copying over head keys to workers')
 for ip in ips:
+    print(' ', ip)
+    sys.stdio.flush()
     cmd = 'scp .ssh/id_rsa.pub '+ip+':.ssh'
     subprocess.run(shlex.split(cmd))
     cmd = 'scp .ssh/id_rsa '+ip+':.ssh'
@@ -26,6 +33,9 @@ for ip in ips:
     subprocess.run(shlex.split(cmd))
 
 # test it out                                                                                                                                                                
+print('Testing ssh one final time')
 for ip in ips:
+    print(' ', ip)
+    sys.stdio.flush()
     cmd = 'ssh '+ip+' echo foo'
     subprocess.run(shlex.split(cmd))
