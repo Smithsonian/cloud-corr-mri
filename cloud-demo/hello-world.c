a4313b3f8a0ff923a906c53691b06e01bf7e1c05
Greg Lindahl <lindahl@pbm.com>
2022-08-22 02:50:48 +0000

    add hostname

--- cloud-demo/hello-world.c
+++ cloud-demo/hello-world.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <mpi.h>
+#include <unistd.h>
+#include <limits.h>
 
 int main(int argc, char **argv)
 {
    int core;
+   char hostname[HOST_NAME_MAX+1];
    
    MPI_Init(&argc,&argv);
    MPI_Comm_rank(MPI_COMM_WORLD, &core);
-     
-   printf("Hello World from Core %d\n", core);
+
+   gethostname(hostname, HOST_NAME_MAX+1);
+   printf("Hello World from Core %d hostname %s\n", core, hostname);
             
    MPI_Finalize();
 }
